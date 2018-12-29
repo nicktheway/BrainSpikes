c1bfbc718e6ca09d514ceeec667d06830ec24c09
nicktheway <nikospa96@hotmail.gr>
2018-12-30 00:27:36 +0200

    Some more charactericstics

--- Code/classif.m
+++ Code/classif.m
@@ -1,16 +1,19 @@
 %% spike classification
 maxA = cell(N, 1);
 zc = cell(N, 1);
+maxPos = cell(N, 1);
 sums = cell(N, 1);
 energy = cell(N, 1);
 for j = 1:N
     maxA{j} = zeros(spikeNumEst(j), 1);
     zc{j} = zeros(spikeNumEst(j), 1);
+    maxPos{j} = zeros(spikeNumEst(j), 1);
     sums{j} = zeros(spikeNumEst(j), 1);
     energy{j} = zeros(spikeNumEst(j), 1);
     for i = 1:spikeNumEst(j)
         spike = spikeEst{j}(i, :);
         maxA{j}(i) = max(spike)-min(spike);
+        [~, maxPos{j}(i)] = max(spike);
         zc{j}(i) = sum(spike(1:end-1) <= 0 & spike(2:end) > 0) + sum(spike(1:end-1) > 0 & spike(2:end) > 0);
         sums{j}(i) = sum(spike);
         F = fft(spike);
@@ -31,7 +34,9 @@ for j = 1:N
     Data{j}(:,1) = zc{j}(valid_idx);
     Data{j}(:,2) = maxA{j}(valid_idx);
     Data{j}(:,3) = sums{j}(valid_idx);
-    Data{j}(:,4) = energy{j}(valid_idx);
+    Data{j}(:,4) = maxPos{j}(valid_idx);
+    Data{j}(:,5) = energy{j}(valid_idx);
+    
     group{j}(:,1) = spike_class{j}(found);
     gscatter(zc{j}(valid_idx), maxA{j}(valid_idx), spike_class{j}(found))
 end
@@ -39,7 +44,7 @@ end
 %% Classification
 pcs = zeros(4, 1);
 for i=1:N
-    pcs(i) = MyClassify(Data{i}(:, [1,2,3,4]), group{i});
+    pcs(i) = MyClassify(Data{i}(:, [1,2,3,4,5]), group{i});
 end
 %{
 %% Useful characteristics
