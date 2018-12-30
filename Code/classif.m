b2d98eb1b572999612897321a57fe2f9758c5b20
nicktheway <nikospa96@hotmail.gr>
2018-12-30 20:39:29 +0200

    Almost done

--- Code/classif.m
+++ Code/classif.m
@@ -1,14 +1,14 @@
 %% spike classification
 maxA = cell(N, 1);
 zc = cell(N, 1);
-maxFr = cell(N, 1);
+WLenergy = cell(N, 1);
 means = cell(N, 1);
 energy = cell(N, 1);
 cycles = cell(N, 1);
 for j = 1:N
     maxA{j} = zeros(spikeNumEst(j), 1);
     zc{j} = zeros(spikeNumEst(j), 1);
-    maxFr{j} = zeros(spikeNumEst(j), 1);
+    WLenergy{j} = zeros(spikeNumEst(j), 1);
     means{j} = zeros(spikeNumEst(j), 1);
     energy{j} = zeros(spikeNumEst(j), 1);
     cycles{j} = zeros(spikeNumEst(j), 1);
@@ -18,8 +18,9 @@ for j = 1:N
         zc{j}(i) = sum(spike(1:end-1) <= 0 & spike(2:end) > 0) + sum(spike(1:end-1) > 0 & spike(2:end) > 0);
         means{j}(i) = mean(spike);
         F = fft(spike);
+        WL = cwt(spike);
         energy{j}(i) = sum(F.*conj(F));
-        maxFr{j}(i) = max(F.*conj(F));
+        WLenergy{j}(i) = sum(sum(WL.*conj(WL)));
         cycles{j}(i) = mean(diff(spike));
     end
 end
@@ -35,7 +36,7 @@ for j = 1:N
     Data{j}(:,1) = zc{j}(valid_idx);
     Data{j}(:,2) = maxA{j}(valid_idx);
     Data{j}(:,3) = means{j}(valid_idx);
-    Data{j}(:,4) = maxFr{j}(valid_idx);
+    Data{j}(:,4) = WLenergy{j}(valid_idx);
     Data{j}(:,5) = energy{j}(valid_idx);
     Data{j}(:,6) = cycles{j}(valid_idx);
     % Normalize
@@ -43,20 +44,28 @@ for j = 1:N
     group{j}(:,1) = spike_class{j}(found);
 end
 
+%{
+%% PCA
+for j=1:N
+    [coeff,score,latent,tsquared,explained] = pca(Data{j});
+    Data{j} = score;
+end
+%}
+
 %% Scatter plot
 figure('Name', 'Scatter plots')
 for j = 1:N
     subplot(2,2, j)
-    gscatter(Data{j}(:,2), Data{j}(:,5), group{j})
+    gscatter(Data{j}(:,4), Data{j}(:,5), group{j})
     title(sprintf("Data Eval %d", j));
-    xlabel("Max peak2peak")
+    xlabel("Sum of wavelet coeffs energy")
     ylabel("Energy")
 end
 
 %% Classification
 pcs = zeros(4, 1);
 for i=1:N
-    pcs(i) = MyClassify(Data{i}(:, [1,2,3,4,5,6]), group{i});
+    pcs(i) = MyClassify(Data{i}(:, [1:6]), group{i});
 end
 %{
 %% Useful characteristics
